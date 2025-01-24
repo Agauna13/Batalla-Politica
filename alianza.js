06818284e3752004f25a8d2b11fa683d5b7af3ab
jooseeruu <arjona2908@gmail.com>
2025-01-24 17:29:15 +0100

    objeto alianza

---
+++ alianza.js
@@ -0,0 +1,34 @@
+const alianzas = {
+  BRICS: {
+    Brasil: { Presidente: "Luiz Inácio Lula da Silva" },
+    Rusia: { Presidente: "Vladimir Putin" },
+    India: { Ministro: "Narendra Modi" },
+    China: { Presidente: "Xi Jinping" },
+    Sudáfrica: { Presidente: "Cyril Ramaphosa" },
+  },
+  G7: {
+    "Reino Unido": { Ministro: "Rishi Sunak" },
+    Francia: { Presidente: "Emmanuel Macron" },
+    Japón: { Ministro: "Fumio Kishida" },
+  },
+  OTAN: {
+    "Estados Unidos": { Presidente: "Donald Trump" },
+    "Reino Unido": { Ministro: "Rishi Sunak" },
+    Francia: { Presidente: "Emmanuel Macron" },
+  },
+  "Unión Europea": {
+    España: { Presidente: "Pedro Sánchez" },
+    Francia: { Presidente: "Emmanuel Macron" },
+    Alemania: { Ministro: "Olaf Scholz" },
+    Italia: { Ministro: "Giorgia Meloni" },
+  },
+  Mercosur: {
+    Argentina: { Presidente: "Javier Milei" },
+    Uruguay: { Presidente: "Luis Lacalle Pou" },
+  },
+  ASEAN: {
+    Indonesia: { Presidente: "Joko Widodo" },
+    Filipinas: { Presidente: "Ferdinand Marcos Jr." },
+    Tailandia: { Presidente: "Prayut Chan-o-cha" },
+  },
+};
